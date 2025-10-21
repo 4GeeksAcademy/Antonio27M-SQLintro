44952f86ff8f933e94fbd8f2fbdc71b96b1fce22
Antonio Martinez <153334216+Antonio27M@users.noreply.github.com>
2025-10-21 11:51:01 +0000

    terminado

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -25,72 +25,73 @@
 -- MISSION 1 ¿Cuáles son las primeras 10 observaciones registradas?
 -- Your query here;
 
---SELECT * FROM observations
---LIMIT 10;
+SELECT * FROM observations
+    LIMIT 10;
 
 -- MISSION 2 ¿Qué identificadores de región (region_id) aparecen en los datos?
 -- Your query here;
 
---SELECT DISTINCT region_id FROM Observations;
+SELECT DISTINCT region_id FROM Observations;
 
 -- MISSION 3 ¿Cuántas especies distintas (species_id) se han observado?
 -- Your query here;
 
---SELECT DISTINCT count(species_id) FROM Observations;
+SELECT DISTINCT count(species_id) FROM Observations;
 
 -- MISSION 4 ¿Cuántas observaciones hay para la región con region_id = 2?
 -- Your query here;
 
---SELECT count(species_id) FROM observations
---WHERE species_id = 2;
+SELECT count(species_id) FROM observations
+    WHERE species_id = 2;
 
 -- MISSION 5 ¿Cuántas observaciones se registraron el día 1998-08-08?
 -- Your query here;
 
---SELECT count(observation_date) FROM observations
---WHERE observation_date = "1998-08-08";
+SELECT count(observation_date) FROM observations
+    WHERE observation_date = "1998-08-08";
 
 -- MISSION 6 ¿Cuál es el region_id con más observaciones?
 -- Your query here;
 
---SELECT count(region_id) FROM observations
---GROUP BY region_id;
+SELECT count(region_id) FROM observations
+    GROUP BY region_id;
 
 
 -- MISSION 7 ¿Cuáles son los 5 species_id más frecuentes?
 -- Your query here;
 
---SELECT species_id, count(*) AS contador FROM observations
---GROUP BY species_id
---ORDER BY contador DESC
---LIMIT 5;
+SELECT species_id, count(*) AS contador FROM observations
+    GROUP BY species_id
+    ORDER BY contador DESC
+    LIMIT 5;
 
 -- MISSION 8 ¿Qué especies (species_id) tienen menos de 5 registros?
 -- Your query here;
 
---SELECT species_id, count(*) AS contador FROM observations
---GROUP BY species_id
---HAVING contador < 5; 
+SELECT species_id, count(*) AS contador FROM observations
+    GROUP BY species_id
+    HAVING contador < 5; 
 
 --MISSION 9 ¿Qué observadores (observer) registraron más observaciones?;
 
---SELECT observer, count(*) AS contador FROM observations
---GROUP BY observer
---ORDER BY contador DESC
---LIMIT 5;
+SELECT observer, count(*) AS contador FROM observations
+    GROUP BY observer
+    ORDER BY contador DESC
+    LIMIT 5;
 
 --MISSION 10 Muestra el nombre de la región (regions.name) para cada observación.;
 
---SELECT regions.name FROM regions
---JOIN Observations ON Observations.id = regions.id;
+SELECT regions.name FROM regions
+    JOIN Observations ON Observations.id = regions.id;
 
 --MISSION 11 Muestra el nombre científico de cada especie registrada (species.scientific_name).;
 
---SELECT species.scientific_name FROM species
---JOIN observations ON observations.id = species.id;
+SELECT species.scientific_name FROM species
+    JOIN observations ON observations.id = species.id;
 
 --MISSION 12 ¿Cuál es la especie más observada por cada región?;
+
 SELECT regions.name, species.scientific_name, count(*) AS contador FROM observations
-JOIN regions ON regions.id = Observations.id
-JOIN Species ON species.id = observations.id
-GROUP BY regions, contador ASC;
+    JOIN regions ON regions.id = Observations.region_id
+    JOIN Species ON species.id = observations.species_id
+    GROUP BY regions.name;
