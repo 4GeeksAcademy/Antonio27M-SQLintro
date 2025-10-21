e6d904cf2c7a65db47dccf5beb3f712f041ab3bb
Antonio Martinez <153334216+Antonio27M@users.noreply.github.com>
2025-10-21 09:20:35 +0000

    actu

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -5,9 +5,9 @@
 -- ✅ Suggestions:
 -- 1) Always end each SQL query with a semicolon `;`
 -- 2) Ensure comments are well-formed:
---    - Use `--` for single-line comments only
+--    - Use `--` for single-line comments only;
 --    - Avoid inline comments after queries
---    - Do not use `/* */` multi-line comments, as they may break execution
+--    - Do not use `/* */` multi-line comments, as they may break execution;
 
 -- -----------------------------------------------
 -- queries.sql
@@ -15,38 +15,69 @@
 -- directly below the corresponding instruction
 -- -----------------------------------------------
 
-SELECT * FROM regions;
-SELECT * FROM species;
-SELECT * FROM climate;
-SELECT * FROM observations;
+--SELECT * FROM regions;
+--SELECT * FROM species;
+--SELECT * FROM climate;
+--SELECT * FROM observations;
 
+--python src/app.py;
 
--- MISSION 1
+-- MISSION 1 ¿Cuáles son las primeras 10 observaciones registradas?
 -- Your query here;
 
--- MISSION 2
+--SELECT * FROM observations
+--LIMIT 10;
+
+-- MISSION 2 ¿Qué identificadores de región (region_id) aparecen en los datos?
 -- Your query here;
 
+--SELECT DISTINCT region_id FROM Observations;
 
--- MISSION 3
+-- MISSION 3 ¿Cuántas especies distintas (species_id) se han observado?
 -- Your query here;
 
+--SELECT DISTINCT count(species_id) FROM Observations;
 
--- MISSION 4
+-- MISSION 4 ¿Cuántas observaciones hay para la región con region_id = 2?
 -- Your query here;
 
+--SELECT count(species_id) FROM observations
+--WHERE species_id = 2;
 
--- MISSION 5
+-- MISSION 5 ¿Cuántas observaciones se registraron el día 1998-08-08?
 -- Your query here;
 
+--SELECT count(observation_date) FROM observations
+--WHERE observation_date = "1998-08-08";
 
--- MISSION 6
+-- MISSION 6 ¿Cuál es el region_id con más observaciones?
 -- Your query here;
 
+--SELECT count(region_id) FROM observations
+--GROUP BY region_id;
 
--- MISSION 7
+
+-- MISSION 7 ¿Cuáles son los 5 species_id más frecuentes?
 -- Your query here;
 
+--SELECT species_id, count(*) AS contador FROM observations
+--GROUP BY species_id
+--ORDER BY contador DESC
+--LIMIT 5;
 
--- MISSION 8
+-- MISSION 8 ¿Qué especies (species_id) tienen menos de 5 registros?
 -- Your query here;
+
+--SELECT species_id, count(*) AS contador FROM observations
+--GROUP BY species_id
+--HAVING contador < 5; 
+
+--MISSION 9 ¿Qué observadores (observer) registraron más observaciones?;
+
+--SELECT observer, count(*) AS contador FROM observations
+--GROUP BY observer
+--ORDER BY contador DESC
+--LIMIT 5;
+
+--MISSION 10 Muestra el nombre de la región (regions.name) para cada observación.;
+
